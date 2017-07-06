87a610b4f6edc3a56cda951f703721fff5fb321f
Rui Xue <rxue@ruixue.mobi>
2017-07-06 18:36:47 +0300

    Sth seems crazy but totally legal

---
+++ src/main/java/practice/ocakasi/variables/TestCrazyVariableIdentifiers.java
@@ -0,0 +1,13 @@
+package practice.ocakasi.variables;
+
+public class TestCrazyVariableIdentifiers {
+
+	public static void main(String[] args) {
+		int Integer = 10;
+		Object Object = new Object();
+		System.out.println("Value of the int variable Integer is " + Integer);
+		System.out.println("Value of the Object Object is " + Object);
+		System.out.println("Such variable identifier is legal but totally bad practice");
+	}
+
+}
