0bc52d718a222c2268165e7a23a85a29424845c7
Rui Xue <rxue@ruixue.mobi>
2017-07-07 10:17:40 +0300

    Sth seems crazy but totally legal

---
+++ src/main/java/practice/ocakasi/primitive/autoboxing_vs_unboxing/TestAutoboxingAndUnboxing.java
@@ -0,0 +1,16 @@
+package practice.ocakasi.primitive.autoboxing_vs_unboxing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestAutoboxingAndUnboxing {
+
+	public static void main(String[] args) {
+		byte b = new Byte("10"); //unboxing
+		Integer integer = 100; //autoboxing
+		List<Float> list = new ArrayList<>();
+		list.add(10f); //autoboxing
+		float addedFloat = list.get(0); //unboxing
+	}
+
+}
