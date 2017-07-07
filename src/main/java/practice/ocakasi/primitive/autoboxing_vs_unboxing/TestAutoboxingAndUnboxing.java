bf256ca63b50b5c0171d10cc38f0f241ec7975b8
Rui Xue <rxue@ruixue.mobi>
2017-07-07 12:11:47 +0300

    TestAutoBoxingAndUnboxing gave examples of autoboxing and unboxing

--- src/main/java/practice/ocakasi/primitive/autoboxing_vs_unboxing/TestAutoboxingAndUnboxing.java
+++ src/main/java/practice/ocakasi/primitive/autoboxing_vs_unboxing/TestAutoboxingAndUnboxing.java
@@ -7,9 +7,9 @@ public class TestAutoboxingAndUnboxing {
 
 	public static void main(String[] args) {
 		byte b = new Byte((byte)10); //unboxing
-		Integer integer = 100; //autoboxing
+		Integer integer = 100; //autoboxing i.e. compiler convert it to: Integer integer = Integer.valueOf(100)
 		List<Float> list = new ArrayList<>();
-		list.add(10f); //autoboxing
+		list.add(10f); //autoboxing i.e. compiler convert it to: list.add(Float.valueOf(10f));
 		float addedFloat = list.get(0); //unboxing
 	}
 
