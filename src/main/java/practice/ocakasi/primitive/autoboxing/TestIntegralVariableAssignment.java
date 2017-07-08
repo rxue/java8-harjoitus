61ad1e526ccba9282d9b94d8102232def4080c98
Rui Xue <ruixue.fi@gmail.com>
2017-07-08 23:30:23 +0300

    tricky integral type Variable assignment

--- src/main/java/practice/ocakasi/primitive/autoboxing/TestIntegralVariableAssignment.java
+++ src/main/java/practice/ocakasi/primitive/autoboxing/TestIntegralVariableAssignment.java
@@ -7,7 +7,8 @@ public class TestIntegralVariableAssignment {
 	public static void main(String[] args) {
 		byte b1 = 127;//Range in [-127, 127] => OK
 		byte b2 = 128;//out of range of [-127, 127] => Does not compile! 
-		byte byteSum = 127 + 1;//All operands are literals && byte is smaller than int  => evaluated at compile time => Does not compile
+		byte byteResult1 = 127 - 1;	//All operands are literals && byte is smaller than int => evaluated at compile time && result in byte range => OK 
+		byte byteSum = 127 + 1;		//All operands are literals && byte is smaller than int => evaluated at compile time && result not in byte range => Does not compile
 		char charSum = 65535 + 2;// Same as the above
 		int intSum = 2147483647 + 1;//arithmetic expression assigned to int variable is not evaluated at compile time
 		System.out.println(intSum);//If the syntax errors above is removed, this can compile and run without Exception but printed overflowed result
