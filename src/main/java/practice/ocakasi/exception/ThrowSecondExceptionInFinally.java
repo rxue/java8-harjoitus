52c1e9dd6214b8f6c0c5e279fd5a4ca554e4beb8
Rui Xue <rxue@ruixue.mobi>
2017-07-21 12:28:45 +0300

    Exception practice code and related deep instruction added

--- src/main/java/practice/ocakasi/exception/ThrowSecondExceptionInFinally.java
+++ src/main/java/practice/ocakasi/exception/ThrowSecondExceptionInFinally.java
@@ -2,20 +2,20 @@ package practice.ocakasi.exception;
 
 class FinallyException extends Exception {}
 class SwallowedException extends Exception  {}
-public class ThrowDoubleException {
+public class ThrowSecondExceptionInFinally {
 	public void openDrawbridge() throws Exception {
 		try {
 			throw new SwallowedException();
 		} catch (SwallowedException e) {
 			System.out.println("The caught exception is not handled, i.e. SWALLOWED");
-			throw new Exception("This is a new Exception");
+			throw new Exception("a second Exception");
 		} finally {
 			throw new FinallyException();
 		}
 	}
 	public static void main(String[] arg) {
 		try {
-			new ThrowDoubleException().openDrawbridge();
+			new ThrowSecondExceptionInFinally().openDrawbridge();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
