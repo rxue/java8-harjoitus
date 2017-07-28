265ec70ddb91ddf428f5cc087ea3784cb9a6d838
Rui Xue <rxue@ruixue.mobi>
2017-07-28 12:40:44 +0300

    Special generic type demo for list added

---
+++ src/main/java/practice/ocakasi/java_core_api/DemoSpecialGenericRemoveFromList.java
@@ -0,0 +1,18 @@
+package practice.ocakasi.java_core_api;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class DemoSpecialGenericRemoveFromList {
+
+	public static void main(String[] args) {
+		List<Number> list = new ArrayList<>();
+		list.add(10);
+		list.add(10L);
+		Object o = 10L;
+		list.remove(o);
+		System.out.println("Object o cannot be added directly to the list as its generic type is Number, but it can be removed as the remove method parameter is Object");
+		System.out.println(list.size());
+	}
+
+}
