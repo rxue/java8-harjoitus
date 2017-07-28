c5417eeb77801f72c9739d4dce926fe1e6fdeea9
Rui Xue <rxue@ruixue.mobi>
2017-07-28 12:22:59 +0300

    ListPuzzler added

---
+++ src/main/java/practice/ocakasi/java_core_api/ListPuzzler.java
@@ -0,0 +1,17 @@
+package practice.ocakasi.java_core_api;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListPuzzler {
+
+	public static void main(String[] args) {
+		List<Short> list = new ArrayList<>();
+		for (short i = 0; i < 10; i++) {
+			list.add(i);
+			list.remove(i);
+		}
+		System.out.println(list.size());
+	}
+
+}
