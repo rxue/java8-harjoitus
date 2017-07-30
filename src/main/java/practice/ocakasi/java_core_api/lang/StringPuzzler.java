73d59875a48482435417c3bdda5232cc18d090e7
Rui Xue <rxue@ruixue.mobi>
2017-07-30 21:20:17 +0300

    source code modified

--- src/main/java/practice/ocakasi/java_core_api/lang/StringPuzzler.java
+++ src/main/java/practice/ocakasi/java_core_api/lang/StringPuzzler.java
@@ -9,9 +9,9 @@ public class StringPuzzler {
 	public static void main(String[] args) {
 		String x = "hello";
 		String sub = x.substring(x.length(),x.length());
-		System.out.println("x.subtring(x.length(), x.length()) returns " + sub);
-		String y = "hello";
-		System.out.println("(y.substring(0, y.length()) == y) returns " + y.substring(0, y.length()) == y);
+		//System.out.println("x.subtring(x.length(), x.length()) returns " + sub);
+		String y = "hello" + true;
+		System.out.println("(y.substring(0, y.length()) == y) returns " +  (y.substring(0, y.length()) == y));
 	}
 
 }
