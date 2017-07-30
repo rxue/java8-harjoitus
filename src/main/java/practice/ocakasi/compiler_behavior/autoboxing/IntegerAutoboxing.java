4a5b7e8699d1003e4652b6e6c9df91e8328d2a33
rxue <ruixue.fi@gmail.com>
2017-07-30 13:12:09 +0300

    IntegerAutoboxing added

---
+++ src/main/java/practice/ocakasi/compiler_behavior/autoboxing/IntegerAutoboxing.java
@@ -0,0 +1,10 @@
+package practice.ocakasi.compiler_behavior.autoboxing;
+
+public class IntegerAutoboxing {
+
+	public static void main(String[] args) {
+		Integer autoBoxedInteger = 201;
+		Integer integer = Integer.valueOf(201);
+	}
+
+}
