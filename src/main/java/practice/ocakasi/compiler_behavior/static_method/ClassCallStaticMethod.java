a987cf95c9a0f6ca6518381cccea2bce6d3cff81
rxue <ruixue.fi@gmail.com>
2017-08-02 19:39:37 +0300

    equivalent instruction code

---
+++ src/main/java/practice/ocakasi/compiler_behavior/static_method/ClassCallStaticMethod.java
@@ -0,0 +1,9 @@
+package practice.ocakasi.compiler_behavior.static_method;
+
+public class ClassCallStaticMethod {
+
+	public static void main(String[] args) {
+		System.out.println(Koala.count);
+	}
+
+}
