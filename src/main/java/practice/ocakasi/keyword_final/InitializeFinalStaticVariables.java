51a15ef20c8b982b46bf1b2772097e8c551c604f
Rui Xue <rxue@ruixue.mobi>
2017-08-03 11:12:54 +0300

    java for final static var initialization added

---
+++ src/main/java/practice/ocakasi/keyword_final/InitializeFinalStaticVariables.java
@@ -0,0 +1,15 @@
+package practice.ocakasi.keyword_final;
+
+public class InitializeFinalStaticVariables {
+	public static final String leftRope;
+	public static final String rightRope;
+	//public final String bench;
+	public static final String name = "name";
+	static {
+		leftRope = "left rope";
+		rightRope = "right rope";
+	}
+	public static void main(String[] args) {
+		//static final variable can not be initialized here
+	}
+}
