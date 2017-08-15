8f44d0f6d1499e6d6532317341437e36d241383f
Rui Xue <rxue@ruixue.mobi>
2017-08-15 17:11:21 +0300

    testing on terminal operations

--- src/test/java/practice/ocpkasi/java_core/util/stream/TestTerminalOperations.java
+++ src/test/java/practice/ocpkasi/java_core/util/stream/TestTerminalOperations.java
@@ -1,15 +1,18 @@
 package practice.ocpkasi.java_core.util.stream;
 
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class TestReduce {
+
+public class TestTerminalOperations {
 	@Test
-	public void testReduce() {
-		Stream<CharSequence> stream = Stream.of("w", "o", "l", "f");
-		StringBuilder sb = new StringBuilder();
-		stream.reduce(sb, (s1, s2) -> ((StringBuilder)s1).append(s2));
-		assertEquals("wolf", sb.toString());
+	public void testCallTerminalOperationTwice() {
+		Predicate<? super String> predicate = s -> s.length() > 3;
+		Stream<String> stream = Stream.iterate("-", (s) -> s + s);
+		stream.noneMatch(predicate);
+		assertThrows(IllegalStateException.class, () -> stream.anyMatch(predicate));
+		
 	}
 }
