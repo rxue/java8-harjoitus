ec84adbffeda3e9f11a7c13d94e18d109807f4d7
Rui Xue <rxue@ruixue.mobi>
2017-08-17 16:23:07 +0300

    Testing on primitive stream added

---
+++ src/test/java/practice/ocpkasi/java_core/util/stream/TestPrimitiveStream.java
@@ -0,0 +1,18 @@
+package practice.ocpkasi.java_core.util.stream;
+
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPrimitiveStream {
+	
+	@Test
+	public void testIntStream() {
+		IntStream stream = IntStream.empty();
+		OptionalDouble avg = stream.average();
+		assertEquals(Double.NaN, avg.orElseGet(() -> Double.NaN));
+	}
+
+}
