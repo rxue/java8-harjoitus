107681f9f3d583c6624863b2f593385a80881506
dingding <ruixue.fi@gmail.com>
2017-09-24 14:30:34 +0300

    TestPathMethods.java

--- oca_ocp/src/test/java/ruixue/practice/ocpkasi/java_core/nio/file/TestPathMethods.java
+++ oca_ocp/src/test/java/ruixue/practice/ocpkasi/java_core/nio/file/TestPathMethods.java
@@ -1,5 +1,8 @@
 package ruixue.practice.ocpkasi.java_core.nio.file;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -10,6 +13,12 @@ import org.junit.jupiter.api.Test;
 public class TestPathMethods {
 	
 	@Test
+	public void testNormalize() {
+		Path p = Paths.get("x/y/.././z");
+		assertEquals(Paths.get("x/z"), p.normalize());
+	}
+	
+	@Test
 	public void testResolve() {
 		Path p1 = Paths.get("x");
 		Path p2 = p1.resolve(Paths.get("y"));
@@ -17,4 +26,24 @@ public class TestPathMethods {
 		Path p3 = Paths.get("/y");
 		assertEquals(Paths.get("/y"), p1.resolve(p3));
 	}
+	
+	@Test
+	public void testToRealPathAndToAbsolutePath() {
+		Path symbolicLink = Paths.get("link");
+		Path linkTarget = Paths.get("linkTarget/final");
+		try {
+			Files.createDirectories(linkTarget);
+			Files.createSymbolicLink(symbolicLink, linkTarget);
+			assertNotEquals(symbolicLink.toAbsolutePath(), linkTarget.toAbsolutePath());
+			assertEquals(symbolicLink.toRealPath(), linkTarget.toRealPath());
+			assertNotEquals(symbolicLink.toRealPath(LinkOption.NOFOLLOW_LINKS), linkTarget.toRealPath());
+			Files.delete(symbolicLink);
+			Files.delete(linkTarget);
+			Files.delete(Paths.get("linkTarget"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
