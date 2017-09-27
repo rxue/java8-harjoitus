3c282924e16a0c9d5a5aec68c0cbe4a871b04425
dingding <ruixue.fi@gmail.com>
2017-09-27 23:37:41 +0300

    demo of Console added

---
+++ oca_ocp/src/main/java/ruixue/practice/ocpkasi/java_core/io/DemoReadFromStdin.java
@@ -0,0 +1,48 @@
+package ruixue.practice.ocpkasi.java_core.io;
+
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * This demo demonstrates that the Console takes care of char encoding, whereas 
+ * the InputStream of System.in does not 
+ * 
+ * @author dingding
+ *
+ */
+public class DemoReadFromStdin {
+
+	public static void demoWithInputStream() {
+		System.out.println("Test with InputStream of System.in");
+		InputStream is = System.in;
+		char[] bytes = new char[1024];
+		int i = 0;
+		try {
+			while ((bytes[i++] = (char) is.read()) != -1) {
+				if (bytes[i-1] == '\n') {
+					System.out.println(bytes);
+					break;
+				} 
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void demoWithConsole() {
+		System.out.println("Test with Console");
+		Console c = System.console();
+		String input;
+		input = c.readLine();
+		System.out.println(input);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Plz try with some special encoded chars, such as Chinese etc.");
+		demoWithInputStream();
+		demoWithConsole();
+	}
+
+}
