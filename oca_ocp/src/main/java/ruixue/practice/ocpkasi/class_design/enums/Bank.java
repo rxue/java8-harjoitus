1599e4b74ef23fb7c6c3977d8a63c6592975ffa5
dingding <ruixue.fi@gmail.com>
2017-11-19 20:50:44 +0200

    enum examples added

---
+++ oca_ocp/src/main/java/ruixue/practice/ocpkasi/class_design/enums/Bank.java
@@ -0,0 +1,20 @@
+package ruixue.practice.ocpkasi.class_design.enums;
+enum Currency {
+	DOLLAR, YEN, EURO
+}
+abstract class Provider {
+	protected Currency c = Currency.EURO;
+}
+public class Bank extends Provider {
+	protected Currency c = Currency.DOLLAR;
+	public static void main(String[] args) {
+		String currencyName = null;
+		switch (new Bank().c) {
+			case DOLLAR: currencyName = Currency.DOLLAR.name(); break;
+			case EURO: currencyName = Currency.EURO.name(); break;
+		}
+		System.out.println(currencyName);
+
+	}
+
+}
