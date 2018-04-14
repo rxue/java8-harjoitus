98849fd507a72437a42d363fb203a923e381543d
dingding <ruixue.fi@gmail.com>
2018-04-14 14:53:46 +0300

    isSameFile test cases added

--- oca_ocp/src/test/java/ruixue/practice/ocpkasi/java_core/nio/file/TestFilesIsSameFile.java
+++ oca_ocp/src/test/java/ruixue/practice/ocpkasi/java_core/nio/file/TestFilesIsSameFile.java
@@ -1,5 +1,6 @@
 package ruixue.practice.ocpkasi.java_core.nio.file;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
@@ -11,12 +12,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestFilesIsSameFile {
 	@Test
-	public void testSamePathButNonExists() {
+	public void testSameNonExistentPath() {
+		String fileName = "not_exists";
+		Path p1 = Paths.get(fileName);
+			try {
+				Files.deleteIfExists(p1);
+				Path p2 = Paths.get(fileName);
+				assertTrue(Files.isSameFile(p1, p2));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+	}
+	@Test
+	public void testNormalizedSameNonExistentPath() {
 		Path currentDir = Paths.get(".");
 		Path p1 = currentDir.toAbsolutePath().normalize().resolve("test");
-		Path p2 = Paths.get("test");
+		Path p2 = currentDir.toAbsolutePath().resolve("test");
 		assertThrows(NoSuchFileException.class, () -> Files.isSameFile(p1, p2));
-	
 	}
 
 }
