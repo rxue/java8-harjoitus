52cd20f83bf0c00bf2499a37aee5d4c6c7d1c322
dingding <ruixue.fi@gmail.com>
2018-04-15 23:09:22 +0300

    test methods, which could return null in some corner cases

---
+++ oca_ocp/src/test/java/ruixue/practice/ocpkasi/java_core/nio/file/TestPathMethodsReturnNull.java
@@ -0,0 +1,21 @@
+package ruixue.practice.ocpkasi.java_core.nio.file;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPathMethodsReturnNull {
+	@Test
+	public void testGetRootOfRelativePath() {
+		Path p = Paths.get("../here");
+		assertNull(p.getRoot());
+	}
+	@Test
+	public void testGetParentOfRoot() {
+		Path p = Paths.get("/");
+		assertNull(p.getParent());
+	}
+
+}
