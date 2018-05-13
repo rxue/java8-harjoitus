297b8d52074c2311b10e8d1539ccfb4110914bfa
dingding <ruixue.fi@gmail.com>
2018-05-13 16:53:08 +0300

    added more code

--- error_code/src/main/java/ruixue/ocpkasi/class_design/advanced/syntax/operator/ImproperUseOfInstanceof.java
+++ error_code/src/main/java/ruixue/ocpkasi/class_design/advanced/syntax/operator/ImproperUseOfInstanceof.java
@@ -1,5 +1,7 @@
 package ruixue.ocpkasi.class_design.advanced.syntax.operator;
 
+import java.util.Collection;
+
 class HeavyAnimal {}
 class Hippo extends HeavyAnimal {}
 class Elephant extends HeavyAnimal {}
@@ -8,6 +10,7 @@ public class ImproperUseOfInstanceof {
 	public static void main(String[] args) {
 		Hippo anotherHippo = new Hippo();
 		boolean b5 = anotherHippo instanceof Elephant; //DOES NOT COMPILE
+		boolean bi = anotherHippo instanceof Collection;
 	}
 
 }
