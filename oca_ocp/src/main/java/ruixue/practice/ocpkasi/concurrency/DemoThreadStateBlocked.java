67ee8b3ab3c43b33bcc7cd05d811fccc977de56b
dingding <ruixue.fi@gmail.com>
2018-06-19 23:03:30 +0300

    a fairly good demo for thread with state blocked is added

---
+++ oca_ocp/src/main/java/ruixue/practice/ocpkasi/concurrency/DemoThreadStateBlocked.java
@@ -0,0 +1,23 @@
+package ruixue.practice.ocpkasi.concurrency;
+
+class SynchronizedClass {
+	
+	public synchronized void synchCall(Thread t) {
+		for (long i = 0; i < Integer.MAX_VALUE; i++);
+		if (t != null)
+		System.out.println("The other thread state is " + t.getState());
+	}
+}
+
+public class DemoThreadStateBlocked {
+
+	public static void main(String[] args) {
+		SynchronizedClass s = new SynchronizedClass();
+		Thread t1 = new Thread(() -> s.synchCall(null));
+		Thread t2 = new Thread(() -> s.synchCall(t1));
+		t2.start();
+		for (long i = 0; i < 10000; i++);
+		t1.start();
+	}
+
+}
