9223a5b1c79ceba5cea86d14cf2ab947d830de71
dingding <ruixue.fi@gmail.com>
2018-07-01 00:21:44 +0300

    more on ConcurrentModificationException test added

--- oca_ocp/src/test/java/ruixue/practice/ocpkasi/exceptions/TestConcurrentModificationException.java
+++ oca_ocp/src/test/java/ruixue/practice/ocpkasi/exceptions/TestConcurrentModificationException.java
@@ -3,7 +3,9 @@ package ruixue.practice.ocpkasi.exceptions;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 import java.util.ConcurrentModificationException;
 
 import org.apache.logging.log4j.LogManager;
@@ -23,5 +25,44 @@ public class TestConcurrentModificationException {
 				list.add(current);
 		});
 		logger.info("Exception came from the line, where the for loop is!");
+	}
+	
+	@Test
+	public void testConcurrentModificationExceptionOnSynchronizedCollection() {
+		List<String> list = new ArrayList<>();
+		list.add("hello");
+		list.add("world");
+		list.add("jesus");
+		List<String> synchronizedList = Collections.synchronizedList(list);
+		assertThrows(ConcurrentModificationException.class, () -> {
+			for (String current : synchronizedList)
+				synchronizedList.add(current);
+		});
+		logger.info("Exception came from the line, where the for loop is!");
+	}
+	
+	@Test
+	public void testConcurrentModificationExceptionOnOldVector() {
+		Vector<String> vector = new Vector<>();
+		vector.add("hello");
+		vector.add("world");
+		vector.add("jesus");
+		assertThrows(ConcurrentModificationException.class, () -> {
+			for (String current : vector)
+				vector.add(current);
+		});
+		logger.info("Exception came from the line, where the for loop is!");
+	}
+	
+	@Test
+	public void testConcurrentModificationExceptionOnModification() {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("hello");
+		list.add("world");
+		int i = 0;
+		for (String current : list) {
+			if (i == 0) list.set(++i, "new value");
+			System.out.println(current);
+		}
 	}
 }
