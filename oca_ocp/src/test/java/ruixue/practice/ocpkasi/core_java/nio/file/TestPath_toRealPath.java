fdb6fee1c2bfb0cfecb7c54edbac1f4ac15b6fff
dingding <ruixue.fi@gmail.com>
2018-07-26 23:10:45 +0300

    testPath_toRealPath added

---
+++ oca_ocp/src/test/java/ruixue/practice/ocpkasi/core_java/nio/file/TestPath_toRealPath.java
@@ -0,0 +1,71 @@
+package ruixue.practice.ocpkasi.core_java.nio.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestPath_toRealPath {
+	private static Path symbolicLink;
+	private static Path existingPath;
+	private static Path nonexistentPath;
+	@BeforeAll
+	public static void init() {
+		symbolicLink = Paths.get("link");
+		existingPath = Paths.get("linked_dir/final");
+		nonexistentPath = Paths.get("nonexistingpath");
+		try {
+			Files.createDirectories(existingPath);
+			Files.createSymbolicLink(symbolicLink, existingPath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@AfterAll
+	public static void destroy() {
+		try {
+			Files.delete(symbolicLink);
+			Files.delete(existingPath);
+			Files.delete(Paths.get("linked_dir"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testFollowLink() {
+		try {
+			assertEquals(symbolicLink.toRealPath(), existingPath.toRealPath());
+			assertNotEquals(symbolicLink.toRealPath(LinkOption.NOFOLLOW_LINKS), existingPath.toRealPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void testToRealPathWithNonExistingPath() {
+		try {
+			nonexistentPath.toRealPath();
+		} catch (IOException e) {
+			assertTrue(e instanceof IOException);
+		}
+	}
+//	@Test
+//	public void testGetName_platformDependent() {
+//		FileSystem currentSystem = FileSystems.getDefault(); 
+//		Path p = Paths.get("/x/y");
+//		assertEquals(Paths.get("x"), p.getName(0));
+//		if (currentSystem.toString().contains("Unix"))
+//			assertEquals(Paths.get("E:"), Paths.get("E:", "x", "y"));
+//	}
+	
+}
