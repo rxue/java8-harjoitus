dadb48e0f0b9b1453552a686e8fb8bc9f2ab68a3
Rui Xue <rui.xue@softwarepoint.com>
2018-09-14 13:56:01 +0300

    DemoTryWithResource updated

--- oca_ocp/src/main/java/ruixue/practice/ocpkasi/compiler_behavior/try_with_resource/DemoTryWithResource.java
+++ oca_ocp/src/main/java/ruixue/practice/ocpkasi/compiler_behavior/try_with_resource/DemoTryWithResource.java
@@ -1,26 +1,42 @@
 package ruixue.practice.ocpkasi.compiler_behavior.try_with_resource;
 
-import java.io.Closeable;
+class PrimaryException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+}
+
+class SuppressedWindowException extends Exception {
+	private static final long serialVersionUID = 1L;
+}
+
+class SuppressedDoorException extends Exception {
+	private static final long serialVersionUID = 1L;
+}
 
 public class DemoTryWithResource {
 	static class Door implements AutoCloseable {
 		@Override
-		public void close() {
+		public void close() throws SuppressedDoorException{
 			System.out.println("implicit finally - close the door");
+			throw new SuppressedDoorException();
 		}	
 	}
-	static class Window implements Closeable {
-		public void close() {
+	static class Window implements AutoCloseable {
+		public void close() throws SuppressedWindowException {
 			System.out.println("implicit finally - close the window");
-			throw new RuntimeException();
+			throw new SuppressedWindowException();
 		}
 	}
 
 	public static void main(String[] args) {
 		try(Door d = new Door(); Window w = new Window()) {
 			System.out.println("try block");
+			throw new PrimaryException();
 		} catch (Exception e) {
-			System.out.println("catch block - exception handler");
+			System.out.println("caught exception is " + (e instanceof PrimaryException ? "PrimaryException" : "not PrimaryException"));
+			Throwable[] suppressedExceptions = e.getSuppressed();
+			for (Throwable t : suppressedExceptions)
+				System.out.println(t);
 		} finally {
 			System.out.println("finally block");
 		}
