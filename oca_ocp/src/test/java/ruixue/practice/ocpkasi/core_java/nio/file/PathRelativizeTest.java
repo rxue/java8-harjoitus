2afbcc9e7e1b83e0d6a859ed1ee621f9968bcb53
Rui Xue <rui.xue@softwarepoint.com>
2018-10-19 09:14:29 +0300

    relativize method added

--- oca_ocp/src/test/java/ruixue/practice/ocpkasi/core_java/nio/file/PathRelativizeTest.java
+++ oca_ocp/src/test/java/ruixue/practice/ocpkasi/core_java/nio/file/PathRelativizeTest.java
@@ -1,14 +1,10 @@
 package ruixue.practice.ocpkasi.core_java.nio.file;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.nio.file.DirectoryNotEmptyException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PathRelativizeTest {
 	@Test
