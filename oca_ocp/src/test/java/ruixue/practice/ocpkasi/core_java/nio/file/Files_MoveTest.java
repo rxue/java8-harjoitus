1479deac6f80aa30ae282efca7ef2724b2a11d37
dingding <ruixue.fi@gmail.com>
2018-10-23 23:43:20 +0300

    move method test

--- oca_ocp/src/test/java/ruixue/practice/ocpkasi/core_java/nio/file/Files_MoveTest.java
+++ oca_ocp/src/test/java/ruixue/practice/ocpkasi/core_java/nio/file/Files_MoveTest.java
@@ -10,24 +10,46 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class Files_MoveTest {
+	private Path sourceFile;
+	@BeforeEach
+	public void init() {
+		sourceFile = Paths.get("originalFile");
+		try {
+			sourceFile = Files.createFile(sourceFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testRenameFile() {
+		Path renamedFile = Paths.get("renamedFile");
+		try {
+			assertNotNull(Files.move(sourceFile, renamedFile));
+			Files.delete(renamedFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	@Test
 	public void testOverwriteExistingFile() {
-		Path source = Paths.get("file_to_overwrite");
 		Path target = Paths.get("target_file");
 		try {
-			Files.createFile(source);
 			Files.createFile(target);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		assertThrows(FileAlreadyExistsException.class, () -> Files.move(source, target));
+		assertThrows(FileAlreadyExistsException.class, () -> Files.move(sourceFile, target));
 		try {
-			Files.delete(source);
+			Files.delete(sourceFile);
 			Files.delete(target);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -36,39 +58,37 @@ public class Files_MoveTest {
 		
 	}
 	@Test
-	public void testMoveFileToExistingEmptyDirectory_REPLACE_EXISTING() {
-		Path source = Paths.get("file_to_move");
-		Path target = Paths.get("target_dir");
+	public void testMoveFileToEmptyDirectory() {
+		Path emptyDirectory = Paths.get("target_emptyDir");
 		try {
-			Files.createFile(source);
-			Files.createDirectory(target);
-			assertTrue(Files.isDirectory(target));
-			Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
-			assertFalse(Files.isDirectory(target));
-			Files.delete(target);
+			Files.createDirectory(emptyDirectory);
+			assert Files.isDirectory(emptyDirectory);
+			Path targetPath = Files.move(sourceFile, emptyDirectory, StandardCopyOption.REPLACE_EXISTING);
+			assertTrue(Files.isRegularFile(targetPath));
+			Files.delete(targetPath);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	@Test
-	public void testMoveFileToExistingNoEmptyDirectory_REPLACE_EXISTING() {
-		Path source = Paths.get("file_to_move");
-		Path targetDir = Paths.get("target_dir");
-		Path fileInTargetDir = targetDir.resolve(targetDir);
-		try {
-			Files.createFile(source);
-			Files.createDirectory(targetDir);
-			Files.createFile(fileInTargetDir);
-			assertThrows(DirectoryNotEmptyException.class, () -> Files.move(source, targetDir, StandardCopyOption.REPLACE_EXISTING));
-			Files.delete(source);
-			Files.delete(fileInTargetDir);
-			Files.delete(targetDir);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	@Test
+//	public void testMoveFileToExistingNoEmptyDirectory_REPLACE_EXISTING() {
+//		Path source = Paths.get("file_to_move");
+//		Path targetDir = Paths.get("target_dir");
+//		Path fileInTargetDir = targetDir.resolve(targetDir);
+//		try {
+//			Files.createFile(source);
+//			Files.createDirectory(targetDir);
+//			Files.createFile(fileInTargetDir);
+//			assertThrows(DirectoryNotEmptyException.class, () -> Files.move(source, targetDir, StandardCopyOption.REPLACE_EXISTING));
+//			Files.delete(source);
+//			Files.delete(fileInTargetDir);
+//			Files.delete(targetDir);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 
 }
