5a8586a75e3d9f166df592fd956612533a9fdc36
Rui Xue <rui.xue@softwarepoint.com>
2018-11-09 13:56:56 +0200

    test on String split

---
+++ oca_ocp/src/test/java/ruixue/practice/advanced/core_java/StringTest.java
@@ -0,0 +1,13 @@
+package ruixue.practice.advanced.core_java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+	@Test
+	public void testSplitStringWithoutSeparator() {
+		assertEquals(1, "hello".split(",").length);
+	}
+
+}
