2ec7de828de5d23a1256d27eab15644286ddf063
dingding <ruixue.fi@gmail.com>
2018-11-11 19:05:34 +0200

    parallel reduction

--- error_code/src/main/java/ruixue/ocpkasi/concurrency/DemoReduceWithMutableIdentity.java
+++ error_code/src/main/java/ruixue/ocpkasi/concurrency/DemoReduceWithMutableIdentity.java
@@ -9,11 +9,17 @@ public class DemoReduceWithMutableIdentity {
 		List<String> list = Arrays.asList("1","2","3","4","5","6","7","8");
 		StringBuilder result = list.parallelStream().reduce(new StringBuilder(), 
 				(StringBuilder b, String s) -> {
-					System.out.println("ACUMULATOR: current builder hashcode: " + b.hashCode() + ", current String to append: " + s);
+					Thread t = Thread.currentThread();
+					System.out.println("ACUMULATOR in thread " + t + " with ID " + t.getId() + "\n"
+							+ "\tCurrent builder has String - \"" + b.toString() + "\"\n with hash code " + b.hashCode() + "\n"
+							+ "\tCurrent String to append is " + s + "\n");
 					return b.append(s);
 				}, 
 				(StringBuilder b1, StringBuilder b2) -> {
-					System.out.println("COMBINER b1 hashcode: " + b1.hashCode() + ", b2 hashcode: " + b2.hashCode());
+					Thread t = Thread.currentThread();
+					System.out.println("COMBINER in thread " + t + " with ID " + t.getId() + "\n"
+							+ "\tb1 has String - \"" + b1.toString() + "\" with hash code " + b1.hashCode() + "\n"
+							+ "\tb2 has String - \"" + b2.toString() + "\" with hash code " + b2.hashCode() + "\n");
 					return b1.append(b2);
 				});
 		System.out.println(result.toString());
