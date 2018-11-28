4e1e73ae478e4326b66ebb0f8bff94a74a023840
Rui Xue <rui.xue@softwarepoint.com>
2018-11-28 11:13:50 +0200

    more demo case added

--- error_code/src/main/java/ruixue/ocpkasi/error/functional_programming/StandardFunctionalInterfaceWithoutGenericType.java
+++ error_code/src/main/java/ruixue/ocpkasi/error/functional_programming/StandardFunctionalInterfaceWithoutGenericType.java
@@ -1,6 +1,7 @@
 package ruixue.ocpkasi.error.functional_programming;
+import java.util.function.Predicate;
 
 public class StandardFunctionalInterfaceWithoutGenericType {
 	static Predicate predicate = t -> "x".equals(t); 
-
+	static Predicate wrongPredicate = t -> "x".equalsIgnoreCase(t); 
 }
