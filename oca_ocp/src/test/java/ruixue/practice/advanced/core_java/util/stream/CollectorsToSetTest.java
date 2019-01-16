e1647de5f293434e120ff04aaacdd489326308a6
Rui Xue <rui.xue@softwarepoint.com>
2019-01-16 10:51:21 +0200

    toSet returns a HashSet by default

---
+++ oca_ocp/src/test/java/ruixue/practice/advanced/core_java/util/stream/CollectorsToSetTest.java
@@ -0,0 +1,24 @@
+package ruixue.practice.advanced.core_java.util.stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+public class CollectorsToSetTest {
+	/**
+	 * toSet() returns a HashSet by default
+	 */
+	@Test
+	public void testToSetReturnHashSet() {
+		List<String> list = Arrays.asList("one", "two", "one", "three");
+		Set<String> set = list.stream().collect(Collectors.toSet());
+		assertTrue(set instanceof HashSet);
+	}
+
+}
