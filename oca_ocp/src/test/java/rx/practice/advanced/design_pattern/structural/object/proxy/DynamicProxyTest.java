f8aaa7f145c20402527bb2a539a1716662f0f0b8
dingding <ruixue.fi@gmail.com>
2019-01-24 00:17:04 +0200

    proxy util added

--- oca_ocp/src/test/java/rx/practice/advanced/design_pattern/structural/object/proxy/DynamicProxyTest.java
+++ oca_ocp/src/test/java/rx/practice/advanced/design_pattern/structural/object/proxy/DynamicProxyTest.java
@@ -1,7 +1,5 @@
 package rx.practice.advanced.design_pattern.structural.object.proxy;
 
-import java.lang.reflect.Proxy;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,11 +7,7 @@ public class DynamicProxyTest {
 
 	@Test
 	public void testProxy() {
-		RealSubject realSubject = new RealSubject();
-		Class<Subject> subjectClass = Subject.class;
-		Subject subjectProxy = (Subject) Proxy.newProxyInstance(
-				subjectClass.getClassLoader(), 
-				new Class<?>[] {subjectClass}, new LoggerHandler(realSubject));
+		Subject subjectProxy = ProxyUtil.getProxy(new RealSubject(), Subject.class);
 		Integer income = subjectProxy.calculateIncome();
 		assertEquals(new Integer(1000), income);
 	}
