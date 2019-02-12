1b3f4a634f95a9a83ec369cc57dd33c181c9428a
dingding <ruixue.fi@gmail.com>
2019-02-12 23:20:21 +0200

    ListHandler added for fun

---
+++ oca_ocp/src/main/java/ruixue/practice/ocpkasi/functional_programming/ListHandler.java
@@ -0,0 +1,14 @@
+package ruixue.practice.ocpkasi.functional_programming;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListHandler {
+	public List<Integer> getSortedDistinctElements(List<Integer> list) {
+		return list.stream()
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
+	}
+	
+}
