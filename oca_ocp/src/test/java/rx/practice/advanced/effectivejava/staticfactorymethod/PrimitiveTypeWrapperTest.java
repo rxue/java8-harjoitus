018ad62d91b4827f70859532dbd6bba241ab5368
Rui Xue <rui.xue@crfhealth.com>
2019-08-13 16:19:51 +0300

    test named

--- oca_ocp/src/test/java/rx/practice/advanced/effectivejava/staticfactorymethod/PrimitiveTypeWrapperTest.java
+++ oca_ocp/src/test/java/rx/practice/advanced/effectivejava/staticfactorymethod/PrimitiveTypeWrapperTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-public class TestPrimitiveTypeWrapper {
+public class PrimitiveTypeWrapperTest {
 
 	@Test
 	@DisplayName("Test the Boolean objects instantiated differently")
