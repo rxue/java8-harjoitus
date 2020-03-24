336b6d6b66a5f28c6ab4fba54dca304aea69ec94
dingding <ruixue.fi@gmail.com>
2020-03-24 13:17:01 +0200

    powerful trim

--- oca_ocp/src/test/java/rx/practice/advanced/java/StringTest.java
+++ oca_ocp/src/test/java/rx/practice/advanced/java/StringTest.java
@@ -15,4 +15,11 @@ public class StringTest {
 		String origin = "hello##_#";
 		assertEquals("hello____", origin.replace("#","_"));
 	}
+	@Test
+	public void testTrimWithReplaceAll() {
+		String original = "hello this is like python";
+		String underscored = "_" +original+ "_";
+		assertEquals(original, underscored.replaceAll("^_|_$", ""));
+		assertEquals(original, original.replaceAll("^_|_$", ""));
+	}
 }
