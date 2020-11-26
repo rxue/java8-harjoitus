25661b108fe1bea28bb1436e04de0babc8a092eb
Rui Xue <rui.xue@sijoitustutkimus.fi>
2020-11-26 11:46:21 +0200

    test on filtered and got an empty colection

---
+++ advanced/core/src/test/java/rx/practice/advanced/learningtests/java/stream/FilterEmptyResultTest.java
@@ -0,0 +1,20 @@
+package rx.practice.advanced.learningtests.java.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class FilterEmptyResultTest {
+    @Test
+    public void testEmptyFilteredResult() {
+        List<String> originalList = Arrays.asList("hello");
+        List<String> result = originalList.stream().filter(s -> s.equals("world")).collect(toList());
+        assertNotNull(result);
+        assertEquals(0, result.size());
+    }
+}
