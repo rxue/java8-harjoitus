36691fb8e0f37408d39786deed3045aa4c81424e
rui <ruixue.fi@gmail.com>
2021-08-26 23:17:22 +0300

    more puzzles added

---
+++ advanced/core/src/main/java/rx/practice/advanced/javapuzzlers/ItsElementary.java
@@ -0,0 +1,10 @@
+package rx.practice.advanced.javapuzzlers;
+
+/**
+ * Puzzle 4: It's elementary
+ */
+public class ItsElementary {
+    public static void main(String[] args) {
+        System.out.println(12345+5432l);
+    }
+}
---
+++ advanced/core/src/main/java/rx/practice/advanced/javapuzzlers/LongDivision.java
@@ -0,0 +1,17 @@
+package rx.practice.advanced.javapuzzlers;
+
+/**
+ * Puzzle 3: Long Division
+ *
+ * This puzzle is called Long Division because it concerns a program that divides
+ * two long values. The dividend represents the number of microseconds in a day;
+ * the divisor, the number of milliseconds in a day. What does the program print?
+ *
+ */
+public class LongDivision {
+    public static void main(String[] args) {
+        final long MICROS_PER_DAY = 24L * 60 * 60 * 1000 * 1000;
+        final long MILLIS_PER_DAY = 24L * 60 * 60 * 1000;
+        System.out.println(MICROS_PER_DAY / MILLIS_PER_DAY);
+    }
+}
