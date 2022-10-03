7194e7e9163971ac3336ad05bc0f91c7355f7b4b
rui <ruixue.fi@gmail.com>
2022-10-03 10:04:32 +0300

    #21 a simple junit test added with the simplest pom.xml configuration

---
+++ legacy/src/test/java/rx/practice/legacy/FirstTest.java
@@ -0,0 +1,11 @@
+package rx.practice.legacy;
+import org.junit.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstTest {
+    @Test
+    public void testTrue() {
+        assertTrue(1==1);
+    }
+}
