0df60d47a54c92c86663d8e02895c1fefa6b4ac1
rui <ruixue.fi@gmail.com>
2022-10-03 10:37:44 +0300

    #21 first integration test added

---
+++ legacy/src/test/java/rx/practice/legacy/FirstIT.java
@@ -0,0 +1,12 @@
+package rx.practice.legacy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstIT {
+    @Test
+    public void testTrue() {
+        assertTrue("This is an integration test with JUnit 4",1==1);
+    }
+}
