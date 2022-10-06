5159beccdddb9766b6579ef864780ee476a5a2ec
Rui Xue <rui.xue@Ruis-MacBook-Pro.local>
2022-10-06 18:33:19 +0300

    The Unbelievable simple program in Effective Java added

--- advanced/core/src/main/java/rx/practice/advanced/effectivejava/generalprogramming/PreferPrimitiveTypesToBoxedPrimitives.java
+++ advanced/core/src/main/java/rx/practice/advanced/effectivejava/generalprogramming/PreferPrimitiveTypesToBoxedPrimitives.java
@@ -4,13 +4,9 @@ import java.util.Comparator;
 
 class PreferPrimitiveTypesToBoxedPrimitives {
     static Comparator<Integer> incorrectNaturalOrder = (i,j) -> (i<j) ? -1 : (i == j ? 0 : 1);
-    static Integer i;
-
     public static void main(String[] args) {
         System.out.println("Result is expected to be 0, but actual result is " + incorrectNaturalOrder.compare(42, 42));
         System.out.println("Result is expected to be 0, but actual result is " + incorrectNaturalOrder.compare(150,150));
-        if (i == 42)
-            System.out.println("Unbelievable");
     }
 
 }
---
+++ advanced/core/src/main/java/rx/practice/advanced/effectivejava/generalprogramming/PreferPrimitiveTypesToBoxedPrimitivesUnbelievable.java
@@ -0,0 +1,9 @@
+package rx.practice.advanced.effectivejava.generalprogramming;
+
+public class PreferPrimitiveTypesToBoxedPrimitivesUnbelievable {
+    static Integer i;
+    public static void main(String[] args) {
+        if (i == 42)
+            System.out.println("Unbelievable");
+    }
+}
