21a5590d887a4871a1c34ecb5509e08013b729ee
rui <ruixue.fi@gmail.com>
2022-12-23 22:54:35 +0200

    #24 groovy test added

---
+++ advanced/core/src/test/groovy/rx/practice/advanced/FirstGroovyTest.groovy
@@ -0,0 +1,12 @@
+package rx.practice.advanced
+
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertSame
+
+class FirstGroovyTest {
+    @Test
+    void "my first test"() {
+        assertSame "x", "x";
+    }
+}
